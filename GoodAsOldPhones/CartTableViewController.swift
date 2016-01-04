e87dd1cab61621d6014975303fd69c6ba745fbd2
Sean Haughey <sdhaughey@gmail.com>
2016-01-04 14:31:22 -0600

    added delete function in cart

--- GoodAsOldPhones/CartTableViewController.swift
+++ GoodAsOldPhones/CartTableViewController.swift
@@ -59,10 +59,9 @@ class CartTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            ordersInCart?.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 
 }
